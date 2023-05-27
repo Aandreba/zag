abaad7b71a22a393ce46b3cf8ecec9288bbd3477
Alex Andreba <aandrebafreelancer@gmail.com>
2023-05-27 14:44:01 +0200

    fixing bugs

--- main.zig
+++ main.zig
@@ -2,7 +2,7 @@ const std = @import("std");
 const zag = @import("src/zag.zig");
 const json = std.json;
 
-pub fn processZag(b: *anyopaque, file_path: ?[]const u8) !void {
+pub fn processZag(b: anytype, file_path: ?[]const u8) !void {
     const file = try zag.ZagFile.parse(std.testing.allocator, if (file_path) |f| f else "zag.json");
     defer file.deinit();
 
