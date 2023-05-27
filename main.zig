bf4b246f892e00d364b4906e8587b3feeabb92ed
Alex Andreba <aandrebafreelancer@gmail.com>
2023-05-27 14:45:54 +0200

    use page allocator

--- main.zig
+++ main.zig
@@ -3,7 +3,7 @@ const zag = @import("src/zag.zig");
 const json = std.json;
 
 pub fn processZag(b: anytype, file_path: ?[]const u8) !void {
-    const file = try zag.ZagFile.parse(std.testing.allocator, if (file_path) |f| f else "zag.json");
+    const file = try zag.ZagFile.parse(std.heap.page_allocator, if (file_path) |f| f else "zag.json");
     defer file.deinit();
 
     const deps = try file.importDeps();
